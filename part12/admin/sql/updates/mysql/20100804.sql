2bda2d353914e4e8c35dd877d44f966dbba9caa2
chdemko <chdemko@f8a2d231-6c6a-0410-89b5-8df3778deb09>
2010-11-21 20:47:28 +0000

    git-svn-id: http://joomlacode.org/svn/helloworld_1_6/trunk@49 f8a2d231-6c6a-0410-89b5-8df3778deb09

--- part12/admin/sql/updates/mysql/20100804.sql
+++
@@ -1,4 +0,0 @@
--- $Id$
-
-ALTER TABLE IGNORE `#__helloworld` ADD `catid` int(11) NOT NULL default '0' 
-
--- part12/admin/views/helloworlds/tmpl/default_foot.php
+++
@@ -1,17 +0,0 @@
-<?php
-/**
- * default head template file for HelloWorlds view of HelloWorld component
- *
- * @version		$Id$
- * @package		Joomla16.Tutorials
- * @subpackage	Components
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
- * @author		Christophe Demko
- * @link		http://joomlacode.org/gf/project/helloworld_1_6/
- * @license		License GNU General Public License version 2 or later
- */
-// No direct access to this file
-defined('_JEXEC') or die('Restricted Access');
-?><tr>
-	<td colspan="3"><?php echo $this->pagination->getListFooter(); ?></td>
-</tr>
