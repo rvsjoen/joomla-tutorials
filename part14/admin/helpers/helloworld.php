c802e7ac8e1bbff2e78e838b1f3fd71852c8770f
chdemko <chdemko@f8a2d231-6c6a-0410-89b5-8df3778deb09>
2010-11-29 22:25:02 +0000

    git-svn-id: http://joomlacode.org/svn/helloworld_1_6/trunk@69 f8a2d231-6c6a-0410-89b5-8df3778deb09

--- part14/admin/helpers/helloworld.php
+++ part14/admin/helpers/helloworld.php
@@ -33,4 +33,29 @@ abstract class HelloWorldHelper
 			$document->setTitle(JText::_('COM_HELLOWORLD_ADMINISTRATION_CATEGORIES'));
 		}
 	}
+	/**
+	 * Get the actions
+	 */
+	public static function getActions($messageId = 0)
+	{
+		$user	= JFactory::getUser();
+		$result	= new JObject;
+
+		if (empty($messageId)) {
+			$assetName = 'com_helloworld';
+		}
+		else {
+			$assetName = 'com_helloworld.message.'.(int) $messageId;
+		}
+
+		$actions = array(
+			'core.admin', 'core.manage', 'core.create', 'core.edit', 'core.delete'
+		);
+
+		foreach ($actions as $action) {
+			$result->set($action,	$user->authorise($action, $assetName));
+		}
+
+		return $result;
+	}
 }
