b0d7b5f362aa70dd6c5ee90bffd0aaf39884b3dc
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-28 17:40:08 +0200

    Update part01 to part04

--- part03/site/views/helloworld/view.html.php
+++ part03/site/views/helloworld/view.html.php
@@ -1,27 +1,20 @@
 <?php
 
 /**
- * @version		$Id: view.html.php 15 2009-11-02 18:37:15Z chdemko $
- * @package		Joomla16.Tutorials
- * @subpackage	Components
+ * @version		$Id:$
+ * @package		Joomla.Tutorials
+ * @subpackage	Component
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
- * @author		Christophe Demko
- * @link		http://joomlacode.org/gf/project/helloworld_1_6/
- * @license		License GNU General Public License version 2 or later
+ * @license		License GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla view library
 jimport('joomla.application.component.view');
 
-/**
- * HTML View class for the HelloWorld Component
- */
 class HelloWorldViewHelloWorld extends JView
 {
-	// Overwriting JView display method
 	function display($tpl = null) 
 	{
 		// Assign data to the view
